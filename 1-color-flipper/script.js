24e5a161e760f304a367b6a72f9d4afd9a7da223
AleksandrMihasenko <aleksandr.mihasenko@gmail.com>
2020-06-12 15:38:35 +0300

    add the main functionality

---
+++ 1-color-flipper/script.js
@@ -0,0 +1,10 @@
+const colors = ['green', 'red', 'rgb(133,50,200)', '#f15025'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+    let randomNumber = Math.floor(Math.random() * colors.length);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];    
+});
