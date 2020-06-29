f3c119fcf699e23daa9678721e2e49615f891b81
AleksandrMihasenko <aleksandr.mihasenko@gmail.com>
2020-06-29 14:38:18 +0300

    add function for closing modal

--- 4-modal/script.js
+++ 4-modal/script.js
@@ -1,20 +1,30 @@
-const modalButton = document.querySelector('.modal');
+const modalButton = document.querySelector('.modal-btn');
 const closeButton = document.querySelector('.close-btn');
 const modal = document.querySelector('.modal');
 const modalOverlay = document.querySelector('.modal-overlay');
+const background = document.querySelector('.container');
 
 
 
 modalButton.addEventListener('click', () => {
+    
     modal.classList.add('close');
     modalOverlay.classList.add('open');
-    document.body.style.backgroundColor = 'rgba(91, 241, 241, 0.664)';
-    document.body.style.backgroundBlendMode = 'overlay';    
+    background.classList.add('open');
+    
 });
 
 closeButton.addEventListener('click', () => {
     modal.classList.remove('close');
     modalOverlay.classList.remove('open');
-    document.body.style.backgroundColor = 'none';
-    document.body.style.backgroundBlendMode = 'normal';
+    background.classList.remove('open');
 });
+
+background.addEventListener('click', () => { 
+    if (event.target.classList.value === 'container open') {
+        modal.classList.remove('close');
+        modalOverlay.classList.remove('open');
+        background.classList.remove('open');
+    }       
+});
+
